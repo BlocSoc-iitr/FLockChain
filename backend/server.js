adbfba3979d8cef4e22381ae1c917efa36124d10
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-12-09 19:49:35 +0530

    Updated Backend

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,32 @@ const express = require("express");
 const app = express();
 const port = 6969;
 const { exec } = require('child_process');
-
-// User Model Params
-var UserRequests = []
+// const math = require('mathjs');
+// const tf = require('@tensorflow/tfjs-node');
 
 // Fully HomoMorphic Encryption
-const SEAL = require('node-seal')
-const seal = await SEAL()
-const schemeType = seal.SchemeType.bfv
-const securityLevel = seal.SecurityLevel.tc128
-const polyModulusDegree = 4096
-const bitSizes = [36, 36, 37]
-const bitSize = 20
-const parms = seal.EncryptionParameters(schemeType)
-parms.setPolyModulusDegree(polyModulusDegree)
-parms.setCoeffModulus(
-  seal.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes))
-)
-parms.setPlainModulus(
-  seal.PlainModulus.Batching(polyModulusDegree, bitSize)
-)
-const context = seal.Context(
-  parms, 
-  true, 
-  securityLevel 
-)
+;(async () => {
+  const SEAL = require('node-seal')
+  const seal = await SEAL()
+  const schemeType = seal.SchemeType.bfv
+  const securityLevel = seal.SecurityLevel.tc128
+  const polyModulusDegree = 4096
+  const bitSizes = [36, 36, 37]
+  const bitSize = 20
+  const parms = seal.EncryptionParameters(schemeType)
+  parms.setPolyModulusDegree(polyModulusDegree)
+  parms.setCoeffModulus(
+    seal.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes))
+  )
+  parms.setPlainModulus(
+    seal.PlainModulus.Batching(polyModulusDegree, bitSize)
+  )
+  const context = seal.Context(
+    parms, 
+    true, 
+    securityLevel 
+  )
+})()
 
 app.use(express.json()); // to get the data in json format
 app.use(express.urlencoded({ extended: true })); // to get the form data
@@ -51,6 +52,8 @@ app.get("/client", (req, res) => {
 app.get("/epoch", (req,res) => {
     console.log("Epoch Done, Starting Evaluation .....");
 
+
+
     cipherText1 = "To get"
     cipherText2 = "Later"
     cipherText_Normalize = "HopeFully :)"
@@ -72,6 +75,11 @@ app.get("/user", (req,res) => {
 
 app.get("/", (req,res) => {
     res.status(200).render("main",{layout : false});
+})
+
+app.post("/model", (req,res) => {
+  console.log("hey therere abvuiberobvoerv");
+  res.status(200).render("model",{layout : false});
 })
 
 app.listen(port, () => {
