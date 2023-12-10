f68bd92a6450ccc9668822b5d79883c0ade279c8
Mihir <mihir6453@gmail.com>
2023-12-10 06:36:49 +0530

    fix: tagline

--- frontend-new/src/Pages/Home/index.jsx
+++ frontend-new/src/Pages/Home/index.jsx
@@ -97,7 +97,7 @@ const Home = () => {
       <div className={styles.content}>
         <div className={styles.title}>FedChain</div>
         <div className={styles.subtitle}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          A Federated Learning network built on Proof of Stake and micro-rollups.
         </div>
         <button className={styles.button} onClick={connect}>
           {connected && account
